a3eb38d5cb45919c569f9610a1653363092291f2
Daniel Fernando Guerrero Ibarra <daniel.guerrero@cern.ch>
2024-05-20 12:22:53 -0500

    New compacts script

--- run_plots.sh
+++ run_plots.sh
@@ -5,34 +5,36 @@ mkdir plots_${VERSION}/
 mkdir plots_${VERSION}/vlllimits_vs_mvll
 mkdir plots_${VERSION}/vlllimits_vs_ctau
 
-#versus ctau(a)
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 10    
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 20
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 30    
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 80    
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 100
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 200   
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 300   
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 800   
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 1000
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 2000  
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 3000  
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 8000  
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 10000 
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 10    --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 30    --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 80    --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 100   --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 300   --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 800   --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 1000  --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 3000  --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 8000  --categ
-python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau 10000 --categ
-#versus m(a)
-python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll 300 
-python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll 700 
-python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll 1000 
-python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll 300   --categ
-python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll 700   --categ
-python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll 1000  --categ
+if [ "$2" == "2" ]; then
+     #versus ctau(a)
+     ctaus=(8 9 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 150 200 250 300 800 1000 2000 3000 8000) #mm
+     for ctau in ${ctaus[@]}
+     do
+     	   python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau ${ctau} 
+     	   python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau ${ctau} --categ
+     done
+     #versus m(a)
+     masses=(200 300 400 500 600 700 800) #gev
+     for ms in ${masses[@]}
+     do
+         python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll ${ms}
+         python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll ${ms}   --categ
+     done
+elif [ "$2" == "10" ]; then
+     #versus ctau(a)
+     ctaus=(10 20 30 80 100 200 300 800 1000 2000 3000 8000 10000) #mm
+     for ctau in ${ctaus[@]}
+     do
+     	   python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau ${ctau} 
+     	   python plot_VLLlimits_1D_mvll.py --unblind --version ${VERSION} --mllp ${MASS} --ctau ${ctau} --categ
+     done
+     #versus m(a)
+     masses=(300 700 1000) #gev
+     for ms in ${masses[@]}
+     do
+         python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll ${ms}
+         python plot_VLLlimits_1D_ctau.py --unblind --version ${VERSION} --mllp ${MASS} --mvll ${ms}   --categ
+     done
+else
+   echo "[ERROR] Mass is not correct!"
+fi
